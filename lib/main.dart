bfcd8088243e2e3330b4a31ef47fc4e9b1991813
Mehmet Ali Günalay <68592606+Mali03@users.noreply.github.com>
2025-08-20 19:02:43 +0300

    Email verification fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynoteapp/firebase_options.dart';
 import 'package:mynoteapp/views/login_view.dart';
 import 'package:mynoteapp/views/register_view.dart';
+import 'package:mynoteapp/views/verify_email_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -32,16 +34,19 @@ class HomePage extends StatelessWidget {
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            // final user = FirebaseAuth.instance.currentUser;
-            // final emailVerified = user?.emailVerified ?? false;
-            // print(user);
-            // if (emailVerified) {
-            //   return Text('Tamamlandi');
-            // } else {
-            //   return const VerifyEmailView();
-            // }
+            final user = FirebaseAuth.instance.currentUser;
 
-            return const LoginView();
+            if (user != null) {
+              if (user.emailVerified) {
+                print("Email is verified");
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
+
+            return const Text("Tamamlandı.");
           default:
             return const CircularProgressIndicator();
         }
